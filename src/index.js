2cb37dd37c39ab283b29993fd4ace38910e92287
Justin Day <justin@unsmoke.me>
2018-05-29 01:31:33 -0400

    Working on ssh2-fs-promise module.

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/**
+ * @file Main access point for ssh2-fs-promise.
+ */
+
+import * as fs from 'ssh2-fs';
+import * as ssh2Connect from 'ssh2-connect';
+
+export function connect(options, callback) {
+  return new Promise((resolve, reject) => {
+    ssh2Connect(options, (err, ssh) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(ssh);
+    });
+  });
+}
+
+export async function mkdir(options, path) {
+  
+}
+
